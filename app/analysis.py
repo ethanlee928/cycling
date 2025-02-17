fd6b7742d38fbdaa4e66ed07f227c75f867937ce
ethanlee928 <ethan2000.el@gmail.com>
2025-02-17 23:18:11 +0800

    refactoring, adding comparison [WIP]

--- app/analysis.py
+++ app/analysis.py
@@ -3,33 +3,9 @@ from datetime import timedelta
 import ollama
 import pandas as pd
 import streamlit as st
+from common import MPS_TO_KPH, MPS_TO_MPH, ZONE_COLORS, ZONES, Colors
 from stqdm import stqdm
 from tcxreader.tcxreader import TCXReader, TCXTrackPoint
-
-MPS_TO_KPH = 3.6
-MPS_TO_MPH = 2.23694
-
-ZONES = [
-    "Active Recovery",
-    "Endurance",
-    "Tempo",
-    "Threshold",
-    "VO2",
-    "Anaerobic Capacity",
-    "Neuromuscular Power",
-]
-
-ZONE_COLORS = ["gray", None, "blue", "green", "orange", "red", "violet"]
-
-
-class Colors:
-    GREY = "#3f3f3f"
-    YELLOW = "#FEDD00"
-    PURPLE = "#6A0DAD"
-    PALE_YELLOW = "#FED172"
-    ORANGE = "#F3742B"
-    DARK_ORANGE = "#B83A14"
-    BLUE_PURPLE = "#231650"
 
 
 def set_colors(value: str, color: str = None) -> str:
@@ -69,6 +45,8 @@ def tcx_to_df(tcx_data, kph: bool) -> pd.DataFrame:
 
 
 def get_zone(power: float, ftp: float) -> int:
+    if not power:
+        return 0
     zones_upper_thresh = [0.55, 0.75, 0.9, 1.05, 1.2, 1.5]
     percentage = power / ftp
     for idx, thresh in enumerate(zones_upper_thresh):
@@ -126,7 +104,6 @@ st.divider()
 if uploaded_file is not None:
     # ----------------- SUMMARY -----------------
     st.header("Workout Summary")
-
     tcx_data = get_tcx_data(uploaded_file)
     df = tcx_to_df(tcx_data, kph=True)
     df["zone"] = df["power"].apply(lambda x: get_zone(x, ftp))
@@ -142,8 +119,8 @@ if uploaded_file is not None:
 
     col1, col2, col3 = st.columns(3)
     col1.metric("Average Speed", f"{df['speed'].mean():.2f} km/h")
-    col2.metric("Calories", f"{round(calories)} kcal")
-    col3.metric("Average Power", f"{round(power_avg)} W")
+    col2.metric("Calories", f"{calories:.0f} kcal")
+    col3.metric("Average Power", f"{power_avg:.0f} W")
 
     st.divider()
 
@@ -187,85 +164,86 @@ if uploaded_file is not None:
     st.divider()
 
     # ----------------- CADENCE -----------------
-    st.header("Cadence")
-    df["elevation_scaled"] = df["elevation"] * (df["cadence"].max() / df["elevation"].max())
-    st.area_chart(
-        df,
-        x="distance",
-        y=["elevation_scaled", "cadence"],
-        x_label="Distance (km)",
-        y_label="Cadence (rpm)",
-        color=[Colors.PURPLE, Colors.GREY],
-        use_container_width=True,
-    )
+    if df["cadence"].notna().sum() > len(df) / 2:
+        st.header("Cadence")
+        df["elevation_scaled"] = df["elevation"] * (df["cadence"].max() / df["elevation"].max())
+        st.area_chart(
+            df,
+            x="distance",
+            y=["elevation_scaled", "cadence"],
+            x_label="Distance (km)",
+            y_label="Cadence (rpm)",
+            color=[Colors.PURPLE, Colors.GREY],
+            use_container_width=True,
+        )
 
-    col1, col2 = st.columns(2)
-    avg_cadence = df[df["cadence"] > 0]["cadence"].mean()
-    col1.metric("Average Cadence", f"{avg_cadence:.0f} rpm")
-    col2.metric("Max Cadence", f"{df['cadence'].max():.0f} rpm")
+        col1, col2 = st.columns(2)
+        avg_cadence = df[df["cadence"] > 0]["cadence"].mean()
+        col1.metric("Average Cadence", f"{avg_cadence:.0f} rpm")
+        col2.metric("Max Cadence", f"{df['cadence'].max():.0f} rpm")
 
-    st.divider()
+        st.divider()
 
     # ----------------- POWER -----------------
+    if df["power"].notna().sum() > len(df) / 2:
+        st.header("Power")
+        st.area_chart(df, x="distance", y="power", x_label="Distance (km)", y_label="Power (W)")
+        zone_counts = round(df["zone"].value_counts(normalize=True).sort_index() * 100, 1)
+        zone_durations = [str(timedelta(seconds=zone_count)) for zone_count in df["zone"].value_counts().sort_index()]
+        zone_counts_df = pd.DataFrame(
+            {
+                "Description": [set_colors(zone, color) for zone, color in zip(ZONES, ZONE_COLORS)],
+                "Range": [get_zone_range(zone, ftp) for zone in range(len(ZONES))],
+                "Zone": zone_counts.index + 1,
+                "Percent": zone_counts.values,
+                "Duration": zone_durations,
+            }
+        )
+        zone_counts_df.set_index("Zone", inplace=True)
+        st.subheader("Zone Distribution")
+        st.bar_chart(
+            data=zone_counts_df,
+            y="Percent",
+            use_container_width=True,
+            horizontal=True,
+        )
+        st.table(zone_counts_df.drop(columns=["Percent"]))
 
-    st.header("Power")
-    st.area_chart(df, x="distance", y="power", x_label="Distance (km)", y_label="Power (W)")
-    zone_counts = round(df["zone"].value_counts(normalize=True).sort_index() * 100, 1)
-    zone_durations = [str(timedelta(seconds=zone_count)) for zone_count in df["zone"].value_counts().sort_index()]
-    zone_counts_df = pd.DataFrame(
-        {
-            "Description": [set_colors(zone, color) for zone, color in zip(ZONES, ZONE_COLORS)],
-            "Range": [get_zone_range(zone, ftp) for zone in range(len(ZONES))],
-            "Zone": zone_counts.index + 1,
-            "Percent": zone_counts.values,
-            "Duration": zone_durations,
-        }
-    )
-    zone_counts_df.set_index("Zone", inplace=True)
-    st.subheader("Zone Distribution")
-    st.bar_chart(
-        data=zone_counts_df,
-        y="Percent",
-        use_container_width=True,
-        horizontal=True,
-    )
-    st.table(zone_counts_df.drop(columns=["Percent"]))
-
-    st.divider()
+        st.divider()
 
     # ----------------- PERFORMANCE COACH -----------------
 
-    st.header("Performance coach")
+        st.header("Performance coach")
 
-    if not st.session_state["summary"]:
-        user_message = {
-            "role": "user",
-            "content": f"Debrief the user's workout in 50 words:\n{zone_counts_df}\n.",
-        }
-        debrief = model_res(messages=[user_message])
-        st.session_state["summary"] = [
-            user_message,
-            {"role": "assistant", "content": debrief},
-        ]
+        if not st.session_state["summary"]:
+            user_message = {
+                "role": "user",
+                "content": f"Debrief the user's workout in 50 words:\n{zone_counts_df}\n.",
+            }
+            debrief = model_res(messages=[user_message])
+            st.session_state["summary"] = [
+                user_message,
+                {"role": "assistant", "content": debrief},
+            ]
 
-    for message in st.session_state["summary"]:
-        if message["role"] == "assistant":
-            with st.chat_message("assistant"):
+        for message in st.session_state["summary"]:
+            if message["role"] == "assistant":
+                with st.chat_message("assistant"):
+                    st.markdown(message["content"])
+
+        for message in st.session_state["messages"]:
+            with st.chat_message(message["role"]):
                 st.markdown(message["content"])
 
-    for message in st.session_state["messages"]:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+        if prompt := st.chat_input("Chat with the performance coach"):
+            st.session_state["messages"].append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
 
-    if prompt := st.chat_input("Chat with the performance coach"):
-        st.session_state["messages"].append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+            with st.chat_message("assistant"):
+                message = st.write_stream(model_res_generator())
+                st.session_state["messages"].append({"role": "assistant", "content": message})
 
-        with st.chat_message("assistant"):
-            message = st.write_stream(model_res_generator())
-            st.session_state["messages"].append({"role": "assistant", "content": message})
-
-    if st.button("Clear chat"):
-        st.session_state["messages"] = []
-        st.session_state["summary"] = []
+        if st.button("Clear chat"):
+            st.session_state["messages"] = []
+            st.session_state["summary"] = []
